02c6f0e3a1a9d646fe8f303a611fcef90fc11556
Nishir <nshelat@usc.edu>
2017-01-21 21:35:04 -0800

    Add for loop

--- content.js
+++ content.js
@@ -2,14 +2,18 @@ var comments;
 var racism = ["nigg", "chink", "redneck", "gypsy", "redskin"];
 var profanity = ["fuck", "ass", "shit", "bastard", "cunt", "dick", "crap", "bitch", "whore", "slut"];
 var homophobia = ["fag", "dyke", "transvestite", "tranny", "sodomite", "homo"];
-var sexism = ["feminazi", "femenazi", "bitch", "slut", "cunt", "whore"]
+var sexism = ["feminazi", "femenazi", "bitch", "slut", "cunt", "whore"];
 
 setTimeout(function(){
 	comments = document.getElementsByClassName("comment-renderer-text-content");
 	for(var i = 0; i < comments.length; i++) {
-		if(comments[i].innerHTML.indexOf(buzzword) != -1) {
-			comments[i].innerHTML = "This comment was suspended because it demonstrates characteristics of online harassment.";
-			comments[i].style = "color: #CE0D00; font-weight: bold";
+		if(racismBool = 1) {
+			for(var j = 0; j < racism.length; j++) {
+			if(comments[i].innerHTML.indexOf(racism[j]) != -1) {
+				comments[i].innerHTML = "This comment was suspended because it demonstrates characteristics of online harassment.";
+				comments[i].style = "color: #CE0D00; font-weight: bold";
+			}
+		}
 		}
 	}
 }, 5000);
