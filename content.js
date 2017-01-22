95b652393b371712c87cb3f6564189c97a5ba333
Nishir <nshelat@usc.edu>
2017-01-21 20:49:17 -0800

    Changed to buzzword

--- content.js
+++ content.js
@@ -1,12 +1,12 @@
 var comments;
-var test_category = "is";
+var buzzword = "is";
 
 setTimeout(function(){
 	comments = document.getElementsByClassName("comment-renderer-text-content");
 	for(var i = 0; i < comments.length; i++) {
-		if(comments[i].innerHTML.indexOf(test_category) != -1) {
+		if(comments[i].innerHTML.indexOf(buzzword) != -1) {
 			comments[i].innerHTML = "This comment was suspended because it demonstrates characteristics of online harassment.";
 			comments[i].style = "color: #CE0D00; font-weight: bold";
 		}
 	}
-}, 2000);
+}, 10000);
