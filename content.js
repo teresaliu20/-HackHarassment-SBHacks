83211dd14ebbfcecec787b23d72f60a483bf8514
Nishir <nshelat@usc.edu>
2017-01-21 22:15:30 -0800

    Added title change and violence array

--- content.js
+++ content.js
@@ -1,20 +1,25 @@
-var comments, rBool, pBool, hBool, sBool;
+var comments, rBool, pBool, hBool, sBool, vBool;
 var racism = ["nigg", "chink", "redneck", "gypsy", "redskin"];
 var profanity = ["fuck", "ass", "shit", "bastard", "cunt", "dick", "crap", "bitch", "whore", "slut", "hoe"];
 var homophobia = ["fag", "dyke", "transvestite", "tranny", "sodomite", "homo"];
 var sexism = ["feminazi", "femenazi", "bitch", "slut", "cunt", "whore", "hoe"];
+var violence = ["kill", "death", "blood", "flesh", "gore"];
 
 rBool = 1;
 pBool = 1;
 hBool = 1;
 sBool = 1;
+vBool = 1;
 
 setInterval(function(){
 	comments = document.getElementsByClassName("comment-renderer-text-content");
+	var numCensored = 0;
 	for(var i = 0; i < comments.length; i++) {
+		var censoredYet = 0;
 		if(rBool == 1) {
 			for(var j = 0; j < racism.length; j++) {
 				if(comments[i].innerHTML.indexOf(racism[j]) != -1) {
+					censoredYet = 1;
 					comments[i].innerHTML = "This comment was suspended because it was deemed racist. Found a variation of: \"" + racism[j] + "\"";
 					comments[i].style = "color: #CE0D00; font-weight: bold";
 				}
@@ -23,6 +28,7 @@ setInterval(function(){
 		if(pBool == 1) {
 			for(var j = 0; j < profanity.length; j++) {
 				if(comments[i].innerHTML.indexOf(profanity[j]) != -1) {
+					censoredYet = 1;
 					comments[i].innerHTML = "This comment was suspended because it was deemed profane. Found a variation of: \"" + profanity[j] + "\"";
 					comments[i].style = "color: #CE0D00; font-weight: bold";
 				}
@@ -31,6 +37,7 @@ setInterval(function(){
 		if(hBool == 1) {
 			for(var j = 0; j < homophobia.length; j++) {
 				if(comments[i].innerHTML.indexOf(homophobia[j]) != -1) {
+					censoredYet = 1;
 					comments[i].innerHTML = "This comment was suspended because it was deemed homophobic. Found a variation of: \"" + homophobia[j] + "\"";
 					comments[i].style = "color: #CE0D00; font-weight: bold";
 				}
@@ -39,10 +46,24 @@ setInterval(function(){
 		if(sBool == 1) {
 			for(var j = 0; j < sexism.length; j++) {
 				if(comments[i].innerHTML.indexOf(sexism[j]) != -1) {
+					censoredYet = 1;
 					comments[i].innerHTML = "This comment was suspended because it was deemed sexist. Found a variation of: \"" + sexism[j] + "\"";
 					comments[i].style = "color: #CE0D00; font-weight: bold";
 				}
 			}
 		}
+		if(vBool == 1) {
+			for(var j = 0; j < violence.length; j++) {
+				if(comments[i].innerHTML.indexOf(violence[j]) != -1) {
+					censoredYet = 1;
+					comments[i].innerHTML = "This comment was suspended because it was deemed violent. Found a variation of: \"" + violence[j] + "\"";
+					comments[i].style = "color: #CE0D00; font-weight: bold";
+				}
+			}
+		}
+		if(censoredYet == 1) {
+			numCensored++;
+		}
 	}
+	document.title = "CENSORED: " + numCensored;
 }, 5000);
