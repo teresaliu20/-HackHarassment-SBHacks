a9e93210b3a7cf095ddc4f0968e1a0f100eb37b9
Nishir <nshelat@usc.edu>
2017-01-21 20:09:19 -0800

    Implemented comment censorship for "is"

--- content.js
+++ content.js
@@ -1,6 +1,12 @@
-// var comments = document.getElementsByTagName('comment-renderer-text-comment');
-// for(var i = 0; i < comments.length(); i++) {
-// 	document.
-// }
+var comments;
+var test_category = "is";
 
-document.getElementsBy
+setTimeout(function(){
+	comments = document.getElementsByClassName("comment-renderer-text-content");
+	for(var i = 0; i < comments.length; i++) {
+		if(comments[i].innerHTML.indexOf(test_category) != -1) {
+			comments[i].innerHTML = "This comment was suspended because it demonstrates characteristics of online harassment.";
+			comments[i].style = "color: #CE0D00; font-weight: bold";
+		}
+	}
+}, 2000);
